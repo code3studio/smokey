f3670383db3e46a750a51515d0a18362f8721548
sparqxi <zhongxi1992@gmail.com>
2024-04-18 10:54:03 -0700

    CA section

--- src/components/how-to-buy/index.tsx
+++ src/components/how-to-buy/index.tsx
@@ -1,16 +1,38 @@
-import { Grid, Typography } from '@mui/material'
-import HowToBuyImg from '../../assets/illustrations/2.png'
+import { Box, Button, Card, CardActionArea, CardHeader, CardMedia, Grid, Typography, useMediaQuery, useTheme } from '@mui/material'
+import HowToBuyImg from '../../assets/illustrations/10.png'
 import MotionWrapper from '../motion-wrapper'
 type Props = {}
 
 const HowToBuy = (_props: Props) => {
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   return (
+    <Box sx={{px:{md:10,sm:2,xs:2}}}>
     <MotionWrapper position={false}>
       <Grid container justifyContent={"center"} mt={4}>
-        <Typography variant="h3" mb={4}>How To Buy</Typography>
+        <Typography variant="h3" mb={4}>$Smokey</Typography>
       </Grid>
-      <img src={HowToBuyImg} alt="illu" width={600} />
+      <Grid container justifyContent={"center"} >
+        <Grid item md={6} sm={8} xs={12} >
+
+      <Card sx={{bgcolor:"#FF9204",borderRadius:12,width:"100%"}} >
+        <CardHeader title="Contract Address"/>
+         
+        <CardMedia component='img' image={"assets/10.png"} height={isMobile ? 300:600}/>
+        <CardActionArea sx={{py:6}}>
+          <Grid container justifyContent={"space-around"} alignItems={"center"} flexDirection={isMobile?"column" :"row"}>
+
+          <Typography mb={isMobile ? 4:0}>{!isMobile ?"35K6HADRfyFig32ubnNHaL9G9WzdPHm4qjhnzDtXtdsa":"35K6HADRfyFig...hnzDtXtdsa"}</Typography>
+          <Button variant='outlined' sx={{fontSize:"26px", textTransform:'capitalize'}}>Copy</Button>
+          </Grid>
+          
+        </CardActionArea>
+      </Card>
+        </Grid>
+      </Grid>
+      {/* <img src={HowToBuyImg} alt="smokey" width={600} /> */}
     </MotionWrapper>
+    </Box>
   )
 }
 
