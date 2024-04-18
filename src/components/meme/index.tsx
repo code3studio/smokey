6469e07a67e25de634694abba67bb6116d48f29d
sparqxi <zhongxi1992@gmail.com>
2024-04-18 11:15:58 -0700

    skipchainged is merged

--- src/components/meme/index.tsx
+++ src/components/meme/index.tsx
@@ -1,6 +1,6 @@
 import { Grid, Typography } from "@mui/material"
 import MotionWrapper from "../motion-wrapper"
-import Meme from '../../assets/illustrations/12.png'
+import Meme from '../../assets/branding/smokey1nobg.png'
 
 type Props = {}
 
@@ -8,7 +8,7 @@ const MemeSection = (_props: Props) => {
   return (
     <MotionWrapper position={true}>
         <Grid container justifyContent={"center"} mt={4}>
-        <Typography variant="h3" mb={4}>Meme</Typography>
+        <Typography variant="h3" mb={4}>Smokey Memes</Typography>
       </Grid>
       <img src={Meme} alt="illu" width={600} />
     </MotionWrapper>
--- src/components/welcome-popup/index.tsx
+++ src/components/welcome-popup/index.tsx
@@ -1,35 +1,36 @@
-import { DialogActions, DialogContent, Grid, Typography } from '@mui/material'
-import Welcome from '../../assets/website/website.png'
-import {motion} from 'framer-motion'
+import { DialogContent, Typography } from '@mui/material';
+import Welcome from '../../assets/website/website.png';
+import { motion } from 'framer-motion';
 
-type Props = {}
+type Props = {};
 
 const WelcomePopup = (_props: Props) => {
   return (
-    <motion.div  initial={{ opacity: 0, scale: 0 }}
-    animate={{ opacity: 1, scale: 1 }}
-    transition={{
-      duration: 0.3,
-      ease: [0, 0.71, 0.2, 1.01],
-      scale: {
-        type: "spring",
-        damping: 5,
-        stiffness: 100,
-        restDelta: 0.001,
-      },
-    }}>
-    <DialogContent>
-        
-        <img src={Welcome} width={"100%"} alt='welcome' style={{borderRadius:'16px'}} />
-    </DialogContent>
-    <DialogActions >
-        <Grid container justifyContent={"center"} alignItems={"center"}>
-
-        <Typography variant='h4' color={"chartreuse"} textAlign={"center"}>Help! Smokey Needs You to save the Forest!</Typography>
-        </Grid>
-    </DialogActions>
+    <motion.div 
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.3,
+        ease: [0, 0.71, 0.2, 1.01],
+        scale: {
+          type: "spring",
+          damping: 5,
+          stiffness: 100,
+          restDelta: 0.001,
+        },
+      }}
+      style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+    >
+      <DialogContent style={{ width: '600px', padding: '16px', minWidth: '100%', textAlign: 'center' }}>
+        <div style={{ width: '95%', margin: '0 auto' }}>
+          <img src={Welcome} alt='welcome' style={{ width: '100%', borderRadius: '16px' }} />
+        </div>
+        <div>
+          <Typography variant='h4' color="#f8eacb" style={{ fontSize: '2rem' }}>Help! Smokey Needs You to save the Forest!</Typography>
+        </div>
+      </DialogContent>
     </motion.div>
-  )
+  );
 }
 
-export default WelcomePopup
+export default WelcomePopup;
